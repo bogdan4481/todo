e0c69f1b788b6c7be1b860e8b3103ba29f4efe37
bogdan4481 <59217907+bogdan4481@users.noreply.github.com>
2020-01-22 17:02:59 +0300

    Первичное создание

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const TodoList = () => {
+    const items = ['Learn React1','Build Awesome App1'];
+    return(
+        <ul>
+            <li>{items [0] }</li>
+            <li>{items [1] }</li>
+        </ul>
+    );
+};
+const AppHeader = () =>{
+    return  <h1> My Todo List </h1>
+};
+
+const SearchPanel = () =>{
+    const searchText = 'mytext';
+    return <input placeholder={searchText}/>
+};
+
+ const App = () => {
+
+     return (
+         <div>
+             <AppHeader/>
+                 <SearchPanel/>
+                     <TodoList/>
+         </div>
+     );
+ };
+
+    ReactDOM.render(<App/>,
+        document.getElementById('root'));
