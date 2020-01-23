d08fd1ce58521ed5bcdd34d609ff70dbc22b643a
bogdan4481 <59217907+bogdan4481@users.noreply.github.com>
2020-01-23 13:01:17 +0300

    вывод всех элементов из массива

--- src/index.js
+++ src/index.js
@@ -5,12 +5,18 @@ import SearchPanel from "./components/search-panel";
 import TodoList from "./components/todo-list";
 
  const App = () => {
+ const todoData = [
+     {label: 'DrincCoffee', important: false},
+     {label: 'Make Awesone App', important: true},
+     {label: 'Have a lunch', important: false},
+     {label: 'End', important: false}
 
+ ];
      return (
          <div>
              <AppHeader/>
                  <SearchPanel/>
-                     <TodoList/>
+                     <TodoList todos={todoData}/>
          </div>
      );
  };
